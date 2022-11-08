112ff32564c9bfec3eb7f8146210bebb2a25b1d7
Wilson <wilson.guayanay@epn.edu.ec>
2022-11-08 11:45:01 -0500

    original2.0

--- src/ejemplo.java
+++ src/ejemplo.java
@@ -2,6 +2,8 @@ public class ejemplo {
     public static void main(String[] args) {
         System.out.println("HOLAAA MUNDO");
 
+        System.out.println("==================");
+        System.out.println("MMVRGA");
 
     }
 }
